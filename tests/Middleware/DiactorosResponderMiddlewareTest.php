8d740f3e426edf7df63a9bdca37c32e027c0e58c
Kocsis Máté <kocsismate@woohoolabs.com>
2015-06-28 20:50:31 +0200

    Fixed coverage error

--- tests/Middleware/DiactorosResponderMiddlewareTest.php
+++ tests/Middleware/DiactorosResponderMiddlewareTest.php
@@ -3,23 +3,17 @@ namespace WoohooLabsTest\Harmony\Middleware;
 
 use FastRoute\Dispatcher;
 use PHPUnit_Framework_TestCase;
-use WoohooLabs\Harmony\Dispatcher\CallbackDispatcher;
-use WoohooLabs\Harmony\Dispatcher\ClassDispatcher;
 use WoohooLabs\Harmony\Harmony;
 use WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware;
-use WoohooLabs\Harmony\Middleware\FastRouteMiddleware;
 use WoohooLabsTest\Harmony\Utils\Container\DummyContainer;
-use WoohooLabsTest\Harmony\Utils\Controller\DummyController;
 use WoohooLabsTest\Harmony\Utils\Diactoros\DummyEmitter;
-use WoohooLabsTest\Harmony\Utils\FastRoute\DummyDispatcher;
 use WoohooLabsTest\Harmony\Utils\Psr7\DummyResponse;
-use WoohooLabsTest\Harmony\Utils\Psr7\DummyServerRequest;
 
 class DiactorosResponderMiddlewareTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::__construct()
-     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::getDispatcher()
+     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::getEmitter()
      */
     public function testConstruct()
     {
@@ -29,8 +23,8 @@ class DiactorosResponderMiddlewareTest extends PHPUnit_Framework_TestCase
     }
 
     /**
-     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::getDispatcher()
-     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::setDispatcher()
+     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::getEmitter()
+     * @covers \WoohooLabs\Harmony\Middleware\DiactorosResponderMiddleware::setEmitter()
      */
     public function testSetDispatcher()
     {
