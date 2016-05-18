de26b8b2476f1b6ee8eac4246cf965f415f48fe9
Kocsis Máté <kocsismate@woohoolabs.com>
2016-05-18 21:18:11 +0200

    Renamed `handlerAttriute` to `actionAttributeName`

--- src/Middleware/FastRouteMiddleware.php
+++ src/Middleware/FastRouteMiddleware.php
@@ -4,8 +4,8 @@ namespace WoohooLabs\Harmony\Middleware;
 use FastRoute\Dispatcher;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use WoohooLabs\Harmony\Exception\MethodNotAllowedException;
-use WoohooLabs\Harmony\Exception\RouteNotFoundException;
+use WoohooLabs\Harmony\Exception\MethodNotAllowed;
+use WoohooLabs\Harmony\Exception\RouteNotFound;
 
 class FastRouteMiddleware
 {
@@ -17,16 +17,16 @@ class FastRouteMiddleware
     /**
      * @var string
      */
-    protected $handlerAttribute;
+    protected $actionAttributeName;
 
     /**
      * @param \FastRoute\Dispatcher $fastRoute
-     * @param string $handlerAttribute
+     * @param string $actionAttributeName
      */
-    public function __construct(Dispatcher $fastRoute = null, $handlerAttribute = "__callable")
+    public function __construct(Dispatcher $fastRoute = null, $actionAttributeName = "__action")
     {
         $this->fastRoute = $fastRoute;
-        $this->handlerAttribute = $handlerAttribute;
+        $this->actionAttributeName = $actionAttributeName;
     }
 
     /**
@@ -34,26 +34,25 @@ class FastRouteMiddleware
      * @param \Psr\Http\Message\ResponseInterface $response
      * @param callable $next
      * @return \Psr\Http\Message\ResponseInterface
-     * @throws \WoohooLabs\Harmony\Exception\MethodNotAllowedException
-     * @throws \WoohooLabs\Harmony\Exception\RouteNotFoundException
+     * @throws \WoohooLabs\Harmony\Exception\MethodNotAllowed
+     * @throws \WoohooLabs\Harmony\Exception\RouteNotFound
      */
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
-        $routeInfo = $this->fastRoute->dispatch($request->getMethod(), $request->getUri()->getPath());
+        $route = $this->fastRoute->dispatch($request->getMethod(), $request->getUri()->getPath());
 
-        switch ($routeInfo[0]) {
-            case Dispatcher::NOT_FOUND:
-                throw new RouteNotFoundException();
-            case Dispatcher::METHOD_NOT_ALLOWED:
-                throw new MethodNotAllowedException();
-            case Dispatcher::FOUND:
-                foreach ($routeInfo[2] as $param => $value) {
-                    $request = $request->withAttribute($param, $value);
-                }
-
-                $request = $request->withAttribute($this->handlerAttribute, $routeInfo[1]);
-                break;
+        if ($route[0] === Dispatcher::NOT_FOUND) {
+            throw new RouteNotFound();
         }
+
+        if ($route[0] === Dispatcher::METHOD_NOT_ALLOWED) {
+            throw new MethodNotAllowed();
+        }
+
+        foreach ($route[2] as $name => $value) {
+            $request = $request->withAttribute($name, $value);
+        }
+        $request = $request->withAttribute($this->actionAttributeName, $route[1]);
 
         return $next($request, $response);
     }
@@ -72,5 +71,21 @@ class FastRouteMiddleware
     public function setFastRoute(Dispatcher $fastRoute)
     {
         $this->fastRoute = $fastRoute;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActionAttributeName()
+    {
+        return $this->actionAttributeName;
+    }
+
+    /**
+     * @param string $actionAttributeName
+     */
+    public function setActionAttributeName($actionAttributeName)
+    {
+        $this->actionAttributeName = $actionAttributeName;
     }
 }
